a11877a4b4272d37ab931b2f1155d49e010102d6
tobslob <kazmobileapp@gmail.com>
2019-04-18 16:20:43 +0100

    [feature #165441619] user signin endpoint

--- server/v1/controllers/user.js
+++ server/v1/controllers/user.js
@@ -1,35 +1,65 @@
-import models from "../model/db";
-import validateUser from "../validation";
-import uuidv4 from "uuid/v4";
-import moment from "moment";
+import models from '../model/db';
+import validate from '../../helper/validation';
+import uuidv4 from 'uuid/v4';
+import moment from 'moment';
+import Helper from '../../helper/helper';
 
 class userController {
-  /**
+    /**
    *
    * @param {req} object
    * @param {res} object
    */
-  static createUser(req, res) {
-    const { error } = validateUser(req.body);
-    if (error)
-      return res.status(422).json({ message: error.details[0].message });
-    const post = {
-      id: uuidv4(),
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      password: req.body.password,
-      status: req.body.status,
-      isAdmin: req.body.isAdmin,
-      createdOn: moment(new Date()),
-      modifiedOn: moment(new Date())
-    };
-    models.User.push(post);
-    return res.status(201).json({
-      status: "201",
-      data: post
-    });
-  }
+    static createUser(req, res) {
+        const { error } = validate.validateUser(req.body);
+        if (error)
+            return res.status(422).json({ message: error.details[0].message });
+        const post = {
+            id: uuidv4(),
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: req.body.password,
+            status: req.body.status,
+            isAdmin: req.body.isAdmin,
+            createdOn: moment(new Date()),
+            modifiedOn: moment(new Date())
+        };
+        models.User.push(post);
+        return res.status(201).json({
+            status: '201',
+            data: post
+        });
+    }
+
+    /**
+   *
+   * @param {req} object
+   * @param {res} object
+   */
+    static loginUser(req, res) {
+        const { error } = validate.validateLogin(req.body);
+        if (error)
+            return res.status(422).json({ message: error.details[0].message });
+        const users = models.User;
+        const user = users.find(specUser => specUser.email === req.body.email);
+        if (!user) return res.status(404).json({
+            status: '404',
+            message: 'no user with such email'
+        });
+        if (!Helper.comparePassword(user.password, req.body.password)) {
+            return res.status(401).json({
+                status: '401',
+                'message': 'Authentication failed'
+            });
+        }
+        const token = Helper.generateToken(user.id);
+        return res.status(200).json({
+            status: '200',
+            token: token,
+            data: user
+        });
+    }
 }
 
 export default userController;
--- server/v1/model/db.js
+++ server/v1/model/db.js
@@ -1,16 +1,16 @@
 let models = {
-  User: [
-    { id: "id" },
-    { email: "email@test.com" },
-    { firstName: "first_name" },
-    { lastName: "last_name" },
-    { password: "password" },
-    { address: "Address" },
-    { status: "status" },
-    { createdOn: Date() },
-    { modifiedOn: Date() },
-    { isAdmin: "false" }
-  ]
+    User: [
+        { id: 'id' },
+        { email: 'email@test.com' },
+        { firstName: 'first_name' },
+        { lastName: 'last_name' },
+        { password: 'password' },
+        { address: 'Address' },
+        { status: 'status' },
+        { createdOn: Date() },
+        { modifiedOn: Date() },
+        { isAdmin: 'false' }
+    ]
 };
 
 module.exports = models;
